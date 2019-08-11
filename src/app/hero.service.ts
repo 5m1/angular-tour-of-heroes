142398a0a70732db89ee135014cab35c9487766b
马卿琦 <mqq@maqingqideMacBook-Pro.local>
2019-08-11 23:23:07 +0800

    fix operation bug

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -27,7 +27,7 @@ export class HeroService {
     this.messageService.add(`HeroService: ${message}`);
   }
 
-  private handleError<T>(operation: 'operation', result?: T) {
+  private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
       console.error(error);
       this.log(`${operation} failed: ${error.message}`);
